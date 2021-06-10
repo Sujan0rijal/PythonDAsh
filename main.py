aaecfbdda24ec64ab94cae8cc67dbb11cd3ec278
GP65 <sujanrijal0@gmail.com>
2021-06-10 09:22:22 +0545

    table

---
+++ main.py
@@ -0,0 +1,6 @@
+'Write a program to print area of rectangle three times by taking user input?'
+
+length = float(input('enter the value of length'))
+breadth = float(input('enter the value of breadth'))
+ares = length*breadth
+for i in range(3):
---
+++ table.py
@@ -0,0 +1,4 @@
+num=int(input('enter your number for table'))
+for i in range(1,11):
+    product=num*i
+    print(product)5
